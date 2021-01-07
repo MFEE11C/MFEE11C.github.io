a803aa31b11db11657c40d6c6e435d3a19b88ab2
jchans <jiangchenghans@gmail.com>
2021-01-07 23:30:55 +0800

    修改 header 顏色與高度，修改 footer 顏色。

--- js/navfoot-v1.js
+++ js/navfoot-v1.js
@@ -4,42 +4,60 @@
 const navTemplate = document.createElement("template");
 navTemplate.innerHTML = `
 
-<nav class="navbar navbar-expand-lg navbar-light" style="padding: 30px; background-color: #d0d9d4">
-    <a class="navbar-brand" href="/">HONKI模板</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<nav
+      class="navbar navbar-expand-lg navbar-dark"
+      style="padding: 30px; background-color: #e83015"
+    >
+      <a class="navbar-brand" href="/">HONKI模板</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">首頁</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/pages/bookstore/merch-list.html">書城</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/pages/activity/bookmarket.html">活動</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/pages/used-books/used-books-index.html">二手書交換</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/pages/old-seasons/os-index.html">過往推薦</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/pages/cart/cart-index.html">購物車</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/pages/members/register.html">會員中心</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://github.com/MFEE11C/MFEE11C.github.io/">Repo</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">首頁</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/pages/bookstore/merch-list.html">書城</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/pages/activity/bookmarket.html">活動</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/pages/used-books/used-books-index.html"
+              >二手書交換</a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/pages/old-seasons/os-index.html"
+              >過往推薦</a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/pages/cart/cart-index.html">購物車</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/pages/members/register.html">會員中心</a>
+          </li>
+          <li class="nav-item active">
+            <a
+              class="nav-link"
+              href="https://github.com/MFEE11C/MFEE11C.github.io/"
+              >Repo</a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
 
 `;
 
@@ -53,8 +71,8 @@ document.getElementById("navbarTemp").appendChild(navTemplate.content);
 const footerTemplate = document.createElement("template");
 footerTemplate.innerHTML = `
 <nav
-      class="navbar navbar-light"
-      style="height: 464px; background-color: #d0d9d4"
+      class="navbar navbar-dark"
+      style="height: 464px; background-color: #e83015"
     >
       <div class="container-fluid">
         <a class="navbar-brand" href="/pages/staff/staff.html">工作人員名單</a>
