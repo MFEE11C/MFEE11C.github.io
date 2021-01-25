4d104a9809daf78809014a5c2a313def4927bb59
jchans <jiangchenghans@gmail.com>
2021-01-26 00:17:13 +0800

    調整 navbar 與 footer

--- js/navfoot-v1.js
+++ js/navfoot-v1.js
@@ -4,60 +4,40 @@
 const navTemplate = document.createElement("template");
 navTemplate.innerHTML = `
 
-<nav
-      class="navbar navbar-expand-lg navbar-dark"
-      style="padding: 30px; background-color: #e83015"
-    >
-      <a class="navbar-brand" href="/">HONKI模板</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<nav class="navbar navbar-expand-lg navbar-dark" style="padding: 20px; background-color: #e83015">
+  <div class="container">
+    <a class="navbar-brand" href="/">LOGO</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">首頁</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/pages/bookstore/merch-list.html">書城</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/pages/activity/bookmarket.html">活動</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/pages/used-books/used-books-index.html"
-              >二手書交換</a
-            >
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/pages/old-seasons/os-index.html"
-              >過往推薦</a
-            >
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/pages/cart/cart-index.html">購物車</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="/pages/members/register.html">會員中心</a>
-          </li>
-          <li class="nav-item active">
-            <a
-              class="nav-link"
-              href="https://github.com/MFEE11C/MFEE11C.github.io/"
-              >Repo</a
-            >
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/pages/bookstore/merch-list.html">所有商品</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/pages/activity/bookmarket.html">活動講座</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/pages/used-books/used-books-index.html">二手書交換</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/pages/old-seasons/os-index.html">過往推薦</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item active mx-2">
+          <a class="nav-link" href="/pages/cart/cart-index.html"><i class="fas fa-shopping-cart"></i></a>
+        </li>
+        <li class="nav-item active mx-2">
+          <a class="nav-link" href="/pages/members/register.html"><i class="fas fa-user"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
 `;
 
@@ -70,14 +50,27 @@ document.getElementById("navbarTemp").appendChild(navTemplate.content);
 
 const footerTemplate = document.createElement("template");
 footerTemplate.innerHTML = `
-<nav
-      class="navbar navbar-dark"
-      style="height: 464px; background-color: #e83015"
-    >
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/pages/staff/staff.html">工作人員名單</a>
-      </div>
-    </nav>
+
+<nav class="navbar navbar-expand-xl navbar-dark" style="height: 280px; background-color: #F3EEE7">
+</nav>
+<nav class="navbar navbar-expand-xl navbar-dark" style="height: 80px; background-color: #e83015">
+  <div class="container">
+    <ul class="navbar-nav mr-auto">
+      <a class="navbar-brand mr-auto" href="/pages/staff/staff.html">© HONKI 2020</a>
+    </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item active mx-2">
+        <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
+      </li>
+      <li class="nav-item active mx-2">
+        <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a>
+      </li>
+      <li class="nav-item active mx-2">
+        <a class="nav-link" href="#"><i class="fab fa-facebook-f"></i></a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 `;
 
